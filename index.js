d0f89416c0cbf104e1cfd77b552c3136e2108a9e
Vinodsk19 <58280917+Vinodsk19@users.noreply.github.com>
2019-11-28 09:24:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(3000); //the server object listens on port 8080
+}).listen(4000); //the server object listens on port 8080
 
-console.log ("Point your broswer to Port http://localhost:3000");
+console.log ("Point your broswer to Port http://localhost:4000");
 
